c97602d49d1113b1fa660da679cc6faa6d233cb7
MaksimZotov <zotovmaksim1254@gmail.com>
2021-06-14 22:56:47 +0300

    added UI fragments

---
+++ app/src/main/java/com/maksimzotov/quiz/view/fragments/SearchOnNameFragment.kt
@@ -0,0 +1,24 @@
+package com.maksimzotov.quiz.view.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.maksimzotov.quiz.R
+import com.maksimzotov.quiz.model.Observer
+import com.maksimzotov.quiz.model.Server
+import data.Data
+
+class SearchOnNameFragment : Fragment(), Observer {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_search_on_name, container, false)
+        Server.setObserver(this)
+        return view
+    }
+
+    override fun getData(data: Data) {
+        TODO()
+    }
+}

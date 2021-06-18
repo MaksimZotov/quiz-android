94ccb24fff4c513ef2017d580caa23bcf80d8d88
MaksimZotov <zotovmaksim1254@gmail.com>
2021-06-18 23:27:27 +0300

    simplified the work with Handler

--- app/src/main/java/com/maksimzotov/quiz/model/communication/ReceiverFromServer.kt
+++ app/src/main/java/com/maksimzotov/quiz/model/communication/ReceiverFromServer.kt
@@ -7,22 +7,16 @@ import data.Data
 
 object ReceiverFromServer : Observable {
     private lateinit var currentObserver: Observer
-    private val CODE_RECEIVED_DATA_FROM_SERVER = 1
 
-    private val handler = object : Handler(Looper.myLooper()!!) {
-        override fun handleMessage(msg: Message) {
-            if (msg.what == CODE_RECEIVED_DATA_FROM_SERVER) {
-                val data = msg.obj as Data
-                currentObserver.getData(data)
-            }
-        }
-    }
+    private val handler = Handler(Looper.myLooper()!!)
 
     override fun setObserver(observer: Observer) {
         currentObserver = observer
     }
 
     fun getData(data: Data) {
-        handler.sendMessage(handler.obtainMessage(CODE_RECEIVED_DATA_FROM_SERVER, -1, -1, data))
+        handler.post {
+            currentObserver.getData(data)
+        }
     }
 }

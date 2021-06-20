206147abdd50290cc369df10571b3d45956e2ea9
MaksimZotov <zotovmaksim1254@gmail.com>
2021-06-20 21:49:25 +0300

    changed myLooper() to getMainLooper()

--- app/src/main/java/com/maksimzotov/quiz/model/communication/ReceiverFromServer.kt
+++ app/src/main/java/com/maksimzotov/quiz/model/communication/ReceiverFromServer.kt
@@ -2,13 +2,12 @@ package com.maksimzotov.quiz.model.communication
 
 import android.os.Handler
 import android.os.Looper
-import android.os.Message
 import data.Data
 
 object ReceiverFromServer : Observable {
     private lateinit var currentObserver: Observer
 
-    private val handler = Handler(Looper.myLooper()!!)
+    private val handler = Handler(Looper.getMainLooper())
 
     override fun setObserver(observer: Observer) {
         currentObserver = observer

bbebec93384bc9f43491441009fb2d2902afca65
MaksimZotov <fsfrgerG425>
2021-07-27 00:45:39 +0300

    changed the threads to the coroutines

--- app/src/main/java/com/maksimzotov/quiz/model/network/Sender.kt
+++ app/src/main/java/com/maksimzotov/quiz/model/network/Sender.kt
@@ -3,25 +3,10 @@ package com.maksimzotov.quiz.model.network
 import data.Data
 import java.io.ObjectOutputStream
 
-class Sender(private val outputStream: ObjectOutputStream) : Thread() {
-    private var runWasLaunchedFromStart = true
-    private lateinit var data: Data
-
-    init {
-        start()
-    }
-
-    override fun run() {
-        if (runWasLaunchedFromStart) {
-            runWasLaunchedFromStart = false
-            return
-        }
-        outputStream.writeObject(data)
-        outputStream.flush()
-    }
+class Sender(private val outputStream: ObjectOutputStream) {
 
     fun sendDataToServer(data: Data) {
-        this.data = data
-        run()
+        outputStream.writeObject(data)
+        outputStream.flush()
     }
 }

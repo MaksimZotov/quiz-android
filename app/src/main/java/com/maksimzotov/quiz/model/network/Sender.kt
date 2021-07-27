d33d633fb03c497a12786475e46264c4ddd7490b
MaksimZotov <fsfrgerG425>
2021-07-27 19:48:47 +0300

    fixed concurrent data sending

--- app/src/main/java/com/maksimzotov/quiz/model/network/Sender.kt
+++ app/src/main/java/com/maksimzotov/quiz/model/network/Sender.kt
@@ -1,12 +1,17 @@
 package com.maksimzotov.quiz.model.network
 
 import data.Data
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import java.io.ObjectOutputStream
 
 class Sender(private val outputStream: ObjectOutputStream) {
+    val mutex = Mutex()
 
-    fun sendDataToServer(data: Data) {
-        outputStream.writeObject(data)
-        outputStream.flush()
+    suspend fun sendDataToServer(data: Data) {
+        mutex.withLock {
+            outputStream.writeObject(data)
+            outputStream.flush()
+        }
     }
 }
